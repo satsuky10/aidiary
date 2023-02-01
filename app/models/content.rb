ee247eb79bc84bcfabbd68bf9e680802df2df475
satsuky <satsuky10@gmail.com>
2023-02-01 22:33:17 +0900

    GPT3APIで文を生成

--- app/models/content.rb
+++ app/models/content.rb
@@ -14,6 +14,22 @@ class Content < ApplicationRecord
   end
 
   def generate_output
-    'むかしむかしあるところに' + input_words + 'がいましたよん'
+    Content.generate_a_sentence(input_words)
+  end
+
+  def self.generate_a_sentence(keyword)
+    require 'net/http'
+    uri = URI.parse("https://api.openai.com/v1/completions")
+    req = Net::HTTP::Post.new(uri)
+    body = {"model": "text-davinci-003", "temperature": 0.2, "max_tokens": 20}
+    keywords_txt = keyword
+    body["prompt"] = "create a sentence in English with: #{keywords_txt}"
+    req.body = body.to_json
+    headers = { "Authorization" => "Bearer #{ENV['OPENAI_KEY']}", "Content-Type" => "application/json" };
+    headers.each{|name, value| req[name] = value}
+    http = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true)
+    response_raw = http.request(req)
+    response = JSON.parse(response_raw.body)
+    response["choices"][0]["text"].strip!
   end
 end
