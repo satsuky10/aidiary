a1c5c11500cf06472b86de891557faa57052d3fb
satsuky <satsuky10@gmail.com>
2023-02-02 13:59:13 +0900

    group関連のルーティング修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  belongs_to :group
   has_many :contents
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "groups#index"
+  root "contents#index"
   get 'home' => 'home#index'
-  resource :group, param: :uid, only: %i(show new create edit update)
   resources :contents, param: :uid
 end
